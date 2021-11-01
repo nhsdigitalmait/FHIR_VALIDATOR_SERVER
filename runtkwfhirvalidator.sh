49145d50f84b380156f6356e4f51d47a1c314d96
Richard Robinson <rrobinson@nhs.net>
2021-11-01 17:56:37 +0000

    updated FHIR Reporting

--- runtkwfhirvalidator.sh
+++ runtkwfhirvalidator.sh
@@ -4,11 +4,12 @@ echo "trustStore = " $trustStore
 echo "trustStorePassword = " $trustStorePassword
 echo "keyStore = " $keyStore
 echo "keyStorePassword = " $keyStorePassword
+echo "terminologyserveraccesstokenlocation = " $terminologyserveraccesstokenlocation
 echo "Making sure output structure is available"
 cd /home/service/data
 tar -xvf /home/service/TKW/config/FHIR_VALIDATOR_SERVER/tkwoutputstructure.tar
 cd /home/service
-javastring="-XX:+UseContainerSupport -XX:MaxRAMPercentage=80.0 -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/FHIR_VALIDATOR_SERVER/tkw-x.multiversion.properties"
+javastring="-Duk.nhs.digital.mait.tkwx.tk.internalservices.validation.hapifhir.r4.terminologyserveraccesstokenlocation=$terminologyserveraccesstokenlocation -XX:+UseContainerSupport -XX:MaxRAMPercentage=80.0 -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/FHIR_VALIDATOR_SERVER/tkw-x.multiversion.properties"
 # decide whether its TLSMA or not
 if [ "$trustStore" == 'default' ]
 then
